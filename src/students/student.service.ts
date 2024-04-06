be4c4aea72b8e6373f2e01429a2f396660c649b5
Camiloriwi <camilomanco2005@gmail.com>
2024-04-05 20:57:43 -0500

    melo del todo ya

--- src/students/student.service.ts
+++ src/students/student.service.ts
@@ -1,8 +1,11 @@
-import { Injectable,Body } from '@nestjs/common';
+
+import { Injectable , Body, Param, HttpException, HttpStatus} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Students } from './student.entity';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
 @Injectable()
 export class StudentService {
 
@@ -23,6 +26,30 @@ export class StudentService {
         const newStudent = new this.studentModel(body);
         return await newStudent.save();
     }
+
+    async findByEmail(email: string): Promise<Students> {
+        return await this.studentModel.findOne({ email: email }).exec();
+    }
+
+
+    async validateUser(email: string, password: string) {
+        const user = await this.findByEmail(email);
+        if (!user) {
+          throw new HttpException('Invalid username or email', HttpStatus.BAD_REQUEST);
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          throw new HttpException('Invalid username or password', HttpStatus.BAD_REQUEST);
+        }
+    
+        const jwt_secret = 'tu_clave_secreta'; // Asegúrate de reemplazar esto con tu clave secreta
+        const token = jwt.sign({ userId: user._id }, jwt_secret, { expiresIn: '1h' });
+        return { message: 'Logged in successfully', token };
+    }
+    
+    
+
 
     async update(id:string, body){
         return await this.studentModel.findByIdAndUpdate(id, body,{new:true}).exec();
--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,5 +1,8 @@
-import { Controller ,Get, Post, Body,  Put, Param,Delete} from '@nestjs/common';
+import { Controller ,Get, Post, Body,  Put, Param,Delete, HttpException, HttpStatus} from '@nestjs/common';
 import { StudentService } from './student.service';
+import * as bcrypt from 'bcrypt';
+// import * as jwt from 'jsonwebtoken';
+const jwt_secret ="###secret";
 
 @Controller('students')
 export class StudentsController {
@@ -15,6 +18,26 @@ export class StudentsController {
     async create(@Body() body){
         return await this.Service.create(body);
     }
+
+    @Post('register')
+    async register(@Body() body) {
+        const { name, email, password } = body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await this.Service.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        return user;
+    }
+
+
+    @Post('login')
+    async login(@Body() body) {
+      return this.Service.validateUser(body.email, body.password);
+    }
+  
+
 
     @Put('path/:_id')
     async updateStudent(@Param('_id') id, @Body() body){
